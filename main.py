231d33821db9ca87d79115eab2713e5014bf08ce
Ray <haackra@gmail.com>
2026-02-15 16:27:15 -0500

    Add Gemini API integration for content generation in main.py

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import os
+
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
-    print("Hello from boot-agent!")
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if not api_key:
+        raise RuntimeError("GEMINI_API_KEY not found")
+
+    client = genai.Client(api_key=api_key)
+    response = client.models.generate_content(
+        model="gemini-3-flash-preview",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
+    )
+    print(response.text)
 
 
 if __name__ == "__main__":

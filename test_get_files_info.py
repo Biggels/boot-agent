1c69b04dfbb9015beb72ec665d12cc500eb5ac1c
Ray <haackra@gmail.com>
2026-02-16 15:52:40 -0500

    Change the ending newlines into separate prints, in case we ever want to add more before them

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -3,22 +3,28 @@ from functions.get_files_info import get_files_info
 
 def main():
     print("Result for current directory:")
-    print(get_files_info("calculator", "."), "\n")
+    print(get_files_info("calculator", "."))
+    print()
 
     print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"), "\n")
+    print(get_files_info("calculator", "pkg"))
+    print()
 
     print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"), "\n")
+    print(get_files_info("calculator", "/bin"))
+    print()
 
     print("Result for '../' directory:")
-    print(get_files_info("calculator", "../"), "\n")
+    print(get_files_info("calculator", "../"))
+    print()
 
     print("Result for 'main.py' non-directory:")
-    print(get_files_info("calculator", "main.py"), "\n")
+    print(get_files_info("calculator", "main.py"))
+    print()
 
     print("Result for '../pysteroids/main.py' non-directory:")
-    print(get_files_info("calculator", "../pysteroids/main.py"), "\n")
+    print(get_files_info("calculator", "../pysteroids/main.py"))
+    print()
 
 
 if __name__ == "__main__":

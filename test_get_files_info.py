fd0240769812b9a94f8e5ebae3ceae4bc32df643
Ray <haackra@gmail.com>
2026-02-16 15:47:15 -0500

    Add line breaks after get_files_info outputs in tests for clarity

--- test_get_files_info.py
+++ test_get_files_info.py
@@ -3,22 +3,22 @@ from functions.get_files_info import get_files_info
 
 def main():
     print("Result for current directory:")
-    print(get_files_info("calculator", "."))
+    print(get_files_info("calculator", "."), "\n")
 
     print("Result for 'pkg' directory:")
-    print(get_files_info("calculator", "pkg"))
+    print(get_files_info("calculator", "pkg"), "\n")
 
     print("Result for '/bin' directory:")
-    print(get_files_info("calculator", "/bin"))
+    print(get_files_info("calculator", "/bin"), "\n")
 
     print("Result for '../' directory:")
-    print(get_files_info("calculator", "../"))
+    print(get_files_info("calculator", "../"), "\n")
 
     print("Result for 'main.py' non-directory:")
-    print(get_files_info("calculator", "main.py"))
+    print(get_files_info("calculator", "main.py"), "\n")
 
     print("Result for '../pysteroids/main.py' non-directory:")
-    print(get_files_info("calculator", "../pysteroids/main.py"))
+    print(get_files_info("calculator", "../pysteroids/main.py"), "\n")
 
 
 if __name__ == "__main__":
